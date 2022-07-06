0dc452b95a36fecfe0236f8da80a49a78db401f5
Justin Garcia <new@NEWs-MacBook-Air.local>
2022-07-05 20:15:36 -0600

    documentation and clean up

--- README.md
+++ README.md
@@ -10,11 +10,8 @@ This is a solution to the [IP address tracker challenge on Frontend Mentor](http
   - [Links](#links)
 - [My process](#my-process)
   - [Built with](#built-with)
-  - [What I learned](#what-i-learned)
-  - [Continued development](#continued-development)
   - [Useful resources](#useful-resources)
 - [Author](#author)
-- [Acknowledgments](#acknowledgments)
 
 ## Overview
 
@@ -46,37 +43,9 @@ Users should be able to:
 - CSS Grid
 - Javascript
 
-
-### What I learned
-
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
-
-To see how you can add code snippets, see below:
-
-```html
-<h1>Some HTML code I'm proud of</h1>
-```
-```css
-.proud-of-this-css {
-  color: papayawhip;
-}
-```
-```js
-const proudOfThisFunc = () => {
-  console.log('🎉')
-}
-```
-
-### Continued development
-
-Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
-
-**Note: Delete this note and the content within this section and replace with your own plans for continued development.**
-
 ### Useful resources
 
-- [Leaflet marker populating form fields and reverse](https://plnkr.co/edit/PTFlun?p=preview&preview) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
-
+- [Leaflet marker populating form fields and reverse](https://plnkr.co/edit/PTFlun?p=preview&preview) - This helped me find how to update marker lat and lng when a form is filled out.
 ## Author
 
 - Website - [Justin Garcia](https://codingjustin.com)
--- scripts.js
+++ scripts.js
@@ -5,52 +5,67 @@ const API_KEY = 'at_O5AamMHaE9r4yUuBRuoweVQjoqvGd'
 window.onload = () => {
     // displays users current IP address
     getInputData('')   
-    
-    let coordinates = JSON.parse(window.localStorage.getItem('coordinates'))
+    // Initialize map
     let map = L.map('map',{
         zoomControl:false
-    }).setView(coordinates, 13);
-
+    }).setView([0,0], 13);
+    // Initialize tile layer
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap'
     }).addTo(map);
-
+    // Initialize icon marker
     var myIcon = L.icon({
         iconUrl: './images/icon-location.svg',
         alt:'Marker',
         iconSize: [38, 45]
     });
 
+    /**
+ * Update map marker location and pan to marker
+ * @param  {Number} data   latitude number
+ * @param  {Number} data   longitude number
+ * @return {Object}   update map object
+ */    
     let latLangHandle = function updateLatLng(lat,lng) {
         map.panTo([lat,lng])
         L.marker(map.getCenter(), {icon: myIcon}).addTo(map);
     }
 
+    /**
+ * Display data on page and pass lat, long values
+ * @param  {Object} data   IP data object
+ * @return {Function}   Pass latitude and longitude values to map
+ */
     function displayData(data) {
         const resultsDiv = document.querySelector('#results')
         const errDiv = resultsDiv.querySelector('.error-display')
         const displayDiv = resultsDiv.querySelector('.data-display')
-        
+        // Hide loading div
         resultsDiv.querySelector('.loading').classList.remove('active')
-    
+        // Display error message
         if( data.code ) {
             errDiv.querySelector('p').innerHTML = data.messages
             errDiv.classList.add('active')
             displayDiv.classList.remove('active')
         }
+        // Display IP address on page
         displayDiv.querySelector('#ip-address p').innerHTML = data.ip
         displayDiv.querySelector('#location p').innerHTML = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`
         displayDiv.querySelector('#timezone p').innerHTML = `UTC ${data.location.timezone}`
         displayDiv.querySelector('#isp p').innerHTML = data.isp
     
         displayDiv.classList.add('active')
+        // Hide error message if showing
         errDiv.classList.remove('active')
-    
-        let latLng = [data.location.lat, data.location.lng]
-        latLangHandle(latLng[0],latLng[1])
-        return window.localStorage.setItem('coordinates', JSON.stringify(latLng))
+        
+        return latLangHandle(data.location.lat,data.location.lng)
     }
-    
+
+/**
+ * IP data fetch
+ * @param  {String} input   IP address
+ * @return {Function}   Pass data object to function
+ */
     async function getInputData (input) {
         const IP_INPUT = input
         const URI = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${IP_INPUT}`
@@ -64,12 +79,33 @@ window.onload = () => {
         }
     }
     
+/**
+ * Get form value
+ * @param  {Element} e The form element
+ * @return {Function}      Pass form value to function
+ */
     function handleSubmit(e) {
         e.preventDefault()
         const value = e.target.querySelector('input').value
+        let resultDivs = document.querySelectorAll('#results > div')
+        resultDivs.forEach( div => {
+            if(div.classList.contains('loading')) {
+                if(!div.classList.contains('active')) {
+                    div.classList.add('active')
+                }
+            } else {
+                div.classList.remove('active')
+                console.log(div)
+            }
+            
+        } )
         return getInputData(value)
     }
     
+/**
+ * Set up form event listener
+ * @return {function}   form submit listener
+ */
     function formSetup() {
         const form = document.querySelector('#ip-form')
         if( !form ) return
